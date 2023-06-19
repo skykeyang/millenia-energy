b4bb0647a8306cc6c7b55698b5b3e637c39e7da5
skykeyang <sohky98@gmail.com>
2023-06-19 19:38:32 +0800

    Completed coming soon page
    Coming soon page complete

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `millenia-energy`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.millenia-energy.com`
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import "./../styles/styles.css"
+// import "../assets/vendor/aos/aos.css"
+import "../assets/vendor/bootstrap/css/bootstrap.min.css"
+import "../assets/vendor/bootstrap-icons/bootstrap-icons.css"
+import "../assets/vendor/boxicons/css/boxicons.min.css"
+// import "../assets/vendor/glightbox/css/glightbox.min.css"
+// import "../assets/vendor/swiper/swiper-bundle.min.css"
 
 function IndexPage() {
   return (
@@ -17,7 +23,7 @@ function IndexPage() {
                   <tbody>
                     <tr>
                       <td className="td1">
-                        Coming Soon
+                      Coming Soon In August!
                       </td>
                     </tr>
                   </tbody>
@@ -30,15 +36,89 @@ function IndexPage() {
       </tr>
       <tr>
         <td>
-          <table cellpadding="0" cellspacing="0" style={{width:'100%', textAlign:'center', background:'#fff'}}>
-          <td>
+          <table style={{width:'100%', textAlign:'center', background:'#fff', marginTop:"15px"}}>
             <tr>
               <td>
-                <table cellpadding="0" cellspacing="0" style= {{width:'960px', margin:'auto', textAlign:'center'}}>
+              <img style= {{width:'5%'}} src={require("../images/logo_nobg.png").default} alt=""></img>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h2>Millenia Energy</h2>
+              </td>
+            </tr>
+            <tr>
+              <td style={{lineHeight:'1.5'}}>1 Yishun Industrial Street 1, #08-16 A POSH Bizhub, Singapore 768160.</td>
+            </tr>
+            <tr>
+              <td style={{height:'20px'}}></td>
+            </tr>
+            <tr>
+                <td>
+                    <table cellpadding="0" cellspacing="0" style={{width:'230px', margin:'auto'}}>
+                        <tbody>
+                        <tr>
+                            <td style={{paddingLeft:'10px', fontSize:'18px', fontWeight:'400', fontFamily:'Roboto Condensed, Arial'}}>
+                                <a href="mailto:info@millenia-energy.com" title="info@millenia-energy.com" style={{whiteSpace: 'nowrap', color: 'black', textDecoration: 'none'}}><i className="bx bx-envelope" style={{padding: '5px'}}></i>info@millenia-energy.com</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                      <td style={{height:'30px'}}>
+                          &nbsp;
+                      </td>
+                    </tr>
+            <tr>
+              <td>
+                <table cellpadding="0" cellspacing="0" style={{width:'100%', textAlign:'center', borderTop:'1px solid #ddd'}}>
                   <tbody>
                     <tr>
                       <td>
-                      <img style= {{width:'30%'}} src={"../images/icon.png"} alt=""></img>
+                        <table cellpadding="0" cellspacing="0" style={{width:'100%', margin:'auto', textAlign:'center'}}>
+                            <tbody>
+                              <tr>
+                                <td>
+                                  <table className="tblContent">
+                                    <tbody>
+                                    <tr>
+                                        <td style={{height:'20px'}}></td>
+                                    </tr>
+                                    <tr>
+                                      <td style={{fontSize:'14px'}}>
+                                          Thank you for visiting us. We're currently upgrading our website.
+                                      </td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{height:'10px'}}></td>
+                                    </tr>
+                                    <tr>
+                                        <td style={{height:'20px'}}></td>
+                                    </tr>
+                                    </tbody>
+                                  </table>
+                                </td>
+                              </tr>
+                            </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </table>
+          {/* <table cellpadding="0" cellspacing="0" style={{width:'100%', textAlign:'center', background:'#fff'}}>
+          <td style={{textAlign:'center'}}>
+            <tr>
+              <td>
+                <table cellpadding="0" cellspacing="0" style= {{margin:'auto', textAlign:'center'}}>
+                  <tbody>
+                    <tr>
+                      <td>
+                      <img style= {{width:'100%'}} src={"../images/icon.png"} alt=""></img>
                       </td>
                     </tr>
                     <tr>
@@ -83,7 +163,7 @@ function IndexPage() {
                   <tbody>
                     <tr>
                       <td>
-                        <table cellpadding="0" cellspacing="0" style={{width:'960px', margin:'auto', textAlign:'center'}}>
+                        <table cellpadding="0" cellspacing="0" style={{width:'100%', margin:'auto', textAlign:'center'}}>
                             <tbody>
                               <tr>
                                 <td>
@@ -116,7 +196,7 @@ function IndexPage() {
               </td>
             </tr>
           </td>
-          </table>
+          </table> */}
         </td>
       </tr>
                 </tbody>
