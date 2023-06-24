8cfa196b5038fd3b811c9cdb2d7734f6f3d9c391
skykeyang <sohky98@gmail.com>
2023-06-25 00:42:22 +0800

    Removed bootstrap dependency
    Imported using cdn script instead

--- src/pages/gatsby-browser.js
+++ src/pages/gatsby-browser.js
@@ -1,7 +1,7 @@
 import IndexPage from "."
 import "../styles/styles.css"
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.min";
+// import "@popperjs/core/dist/umd/popper.min";
 
 export default IndexPage;
