5a91ae1eb6f9efebc70aea7e17c9cdd2c3720ace
skykeyang <sohky98@gmail.com>
2023-07-11 19:10:40 +0800

    Fixed lambda function and api

--- amplify/backend/function/milleniaenergy9b718375/src/index.py
+++ amplify/backend/function/milleniaenergy9b718375/src/index.py
@@ -8,29 +8,30 @@ import os.path
 import googleapiclient.discovery
 
 from google.oauth2 import service_account
+from google.oauth2.service_account import Credentials
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+# from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 s3 = boto3.resource('s3')
-keys = ['credentials.json', 'token.json']
-SERVICE_ACCOUNT_FILE = 'https://amplify-amplify912f78195bc04-staging-80157-deployment.s3.ap-southeast-1.amazonaws.com/service.json'
-# BUCKET_NAME = 'amplify-amplify912f78195bc04-staging-80157-deployment'
-# for KEY in keys:
-#     try:
-#         local_file_name = '/tmp/' + KEY
-#         s3.Bucket(BUCKET_NAME).download_file(KEY, local_file_name)
-#     except botocore.exceptions.ClientError as e:
-#         if e.response['Error']['Code'] == "404":
-#             continue
-#         else:
-#             raise
+keys = ['service.json']
+# SERVICE_ACCOUNT_FILE = 'https://amplify-amplify912f78195bc04-staging-80157-deployment.s3.ap-southeast-1.amazonaws.com/service.json'
+BUCKET_NAME = 'amplify-amplify912f78195bc04-staging-80157-deployment'
+for KEY in keys:
+    try:
+        local_file_name = '/tmp/' + KEY
+        s3.Bucket(BUCKET_NAME).download_file(KEY, local_file_name)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            continue
+        else:
+            raise
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes= SCOPES)
+
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '16-5d-_EaxgZ9P5eD2tDiqrskBOykOdQWERvnohBPyTw'
@@ -50,9 +51,10 @@ def list_metrics():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    # if os.path.exists('/tmp/token.json'):
-    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # # If there are no (valid) credentials available, let the user log in.
+    if os.path.exists('/tmp/service.json'):
+        # creds = Credentials.from_service_account_file('service.json', SCOPES)
+        credentials = service_account.Credentials.from_service_account_file("/tmp/service.json", scopes=SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
     # if not creds or not creds.valid:
     #     if creds and creds.expired and creds.refresh_token:
     #         creds.refresh(Request())
