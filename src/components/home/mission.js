0b54b3bc4642b2f371efbaa048179cfe5ba2bf38
skykeyang <sohky98@gmail.com>
2023-07-18 02:36:21 +0800

    Added animations and cropped banner

--- src/components/home/mission.js
+++ src/components/home/mission.js
@@ -1,30 +1,34 @@
 import React from "react";
+import { useRef } from "react";
 import "./css/mission.css";
 import "../../styles/styles.css";
+import { useInView } from "framer-motion";
 
 // 770px for top section
 // 560px for metrics
 
 export default function Mission() {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
 
     return (
-        <div className="container">
+        <div className="container" ref={ref}>
         <div className="row section-two">
-            <section id='mission' className="missionvision col-sm-6">
+            <section id='mission' className="missionvision col-sm-6" 
+            style={{transform: isInView ? "none" : "translateY(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}}>
                 <h2 className="missiontitle">Millenia Energy</h2>
                 <h3 className="together">Together For A Greener Tomorrow </h3>
                 <h3 className="ourmission">Our Mission</h3>
                 <p className="missioncontent">Our primary mission is centered around the crucial objective of decarbonizing the built environment, recognizing its significant contribution of approximately 40% to global greenhouse gas emissions.
                  <br/><br/> Our goal is to establish ourselves as a prominent player in the South East Asia region by actively developing, investing in, and operating renewable energy
                  <br/><br/> At Millenia Energy, our commitment entails assuming the role of project developer, owner, and operator for solar ventures, effectively alleviating the capital cost constraints faced by our valued clientele.</p>
-                {/* <h3 className="ourmission">Sustainable & Impact Investing (SII)</h3>
-                <p className="missioncontent">
-                We hold the view that the economy, markets, and portfolios are exposed to substantial risks stemming from unsustainable environmental and social justice trends.
-                <br/><br/>Moreover, we firmly believe that clients are not compelled to make a trade-off between long-term portfolio returns and making a positive impact in the real world. 
-                <br/><br/>By constructing portfolios that prioritize sustainability and solutions-oriented approaches, our objective is to provide portfolios that are resilient and future-proof.
-                </p> */}
             </section>
-            <section id='image' className="image-wrapper col-sm-6">
+            <section id='image' className="image-wrapper col-sm-6"
+            style={{transform: isInView ? "none" : "translateY(200px)",
+            opacity: isInView ? 1 : 0,
+            transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"}}>
                 <img className="solar-image" src={require("../../images/solar_image.png").default}></img>
             </section>
         </div>
