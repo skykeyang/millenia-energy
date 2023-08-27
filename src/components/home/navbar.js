43b816297d39724121b0dad25c54dcde5f515706
skykeyang <sohky98@gmail.com>
2023-08-28 02:05:37 +0800

    Added styling to investors dropdown

--- src/components/home/navbar.js
+++ src/components/home/navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
                   <ul className= {`backblurred ${isShowing ? 'showing': null}`} >
                     <li id="about"><Link to ="/about"> about </Link></li>
                     <li id="investors" onMouseEnter = {handleMouseEnter} onMouseLeave={handleMouseLeave}> 
-                                          investors <svg xmlns="http://www.w3.org/2000/svg" width="12" height="9" viewBox="0 0 12 9" fill="none">
+                                          <Link to="/investors">investors</Link> <svg xmlns="http://www.w3.org/2000/svg" width="12" height="9" viewBox="0 0 12 9" fill="none">
                                                       <path d="M5.85217 9L0.656021 0.75L11.0483 0.75L5.85217 9Z" fill="#3A4A01" fill-opacity="0.8"/>
                                                    </svg>
                                           
