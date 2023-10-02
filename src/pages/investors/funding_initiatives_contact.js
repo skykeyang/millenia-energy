bae84cc4f78ebcfda6ad1c0d38166175905a09bf
skykeyang <sohky98@gmail.com>
2023-10-02 21:38:35 +0800

    Complete contact form

--- src/pages/investors/funding_initiatives_contact.js
+++ src/pages/investors/funding_initiatives_contact.js
@@ -1,8 +1,8 @@
 import * as React from "react"
+import axios from "axios"
 import Navbar from "../../components/home/navbar"
 import Footer from "../../components/home/footer"
 import FundingBanner from "../../components/misc/funding_banner"
-import { Link } from 'gatsby'
 import "./../../styles/styles.css"
 import "./../../styles/investors.css"
 import "../../assets/vendor/bootstrap/css/bootstrap.min.css"
@@ -10,6 +10,47 @@ import "../../assets/vendor/bootstrap-icons/bootstrap-icons.css"
 import "../../assets/vendor/boxicons/css/boxicons.min.css"
 
 const FundingInitiativesPage = () => {
+   
+
+    const [name, setName] = React.useState("");
+    const [company, setCompany] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+        console.log(event.target.value);
+    }
+
+    const handleCompanyChange = (event) => {
+        setCompany(event.target.value);
+    }
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handleMessageChange = (event) => {
+        setMessage(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        axios.post("https://og188mg535.execute-api.ap-southeast-1.amazonaws.com/default/SES_Send_Email", 
+        {header:"",body: {senderName: name, senderCompany: company, senderEmail: email, senderMessage: message}})
+            
+
+        .then((response) => {
+            if (response.data.status === 'success') {
+                alert("Message Sent.");
+                
+                // this.context.router.history.push('/success')
+            } else if (response.data.status === 'fail') {
+                alert("Message failed to send.")
+            }
+        })
+    }
+    
+
     return (
         <div>
             <div className="container">
@@ -44,34 +85,34 @@ const FundingInitiativesPage = () => {
                             </div>
                         </div>
                         <div className="col-6 col-md-6">
-                            <form className="contact-form">
+                            <form className="contact-form" onSubmit={handleSubmit}>
                                 <div className="row">
                                     <div className="col-12">
                                         <div className="mb-3">
-                                            <label for="name" className="form-label">Name</label>
-                                            <input type="text" className="form-control" id="name" placeholder="Enter your name"></input>
+                                            <label for="name" className="form-label" onChange={handleNameChange}>Name</label>
+                                            <input type="text" className="form-control" id="name" placeholder="Enter your name" required ></input>
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="mb-3">
-                                            <label for="email" className="form-label">Company</label>
-                                            <input type="email" className="form-control" id="email" placeholder="Enter your email"></input>
+                                            <label for="company" className="form-label" onChange={handleCompanyChange}>Company</label>
+                                            <input type="text" className="form-control" id="company" placeholder="Enter your company name" required ></input>
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="mb-3">
-                                            <label for="phone" className="form-label">Phone</label>
-                                            <input type="text" className="form-control" id="phone" placeholder="Enter your phone number"></input>
+                                            <label for="email" className="form-label" onChange={handleEmailChange}>Email</label>
+                                            <input type="email" className="form-control" id="email" placeholder="Enter your email" required ></input>
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="mb-3">
-                                            <label for="message" className="form-label">Message</label>
-                                            <textarea className="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+                                            <label for="message" className="form-label" onChange={handleMessageChange}>Message</label>
+                                            <textarea className="form-control" id="message" rows="6" placeholder="Enter your message" required ></textarea>
                                         </div>
                                     </div>
                                     <div className="col-12">
-                                        <div className="d-flex justify-content-center">
+                                        <div>
                                             <button type="submit" className="btn btn-primary rounded-pill contact-form-button">SUBMIT</button>
                                         </div>
                                     </div>
