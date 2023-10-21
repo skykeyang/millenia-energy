edd6c0b98b4b8044080312388e25bcfc3bb7d0b9
skykeyang <sohky98@gmail.com>
2023-10-22 04:04:20 +0800

    Complete banners

--- src/components/misc/media-banner.js
+++ src/components/misc/media-banner.js
@@ -9,7 +9,7 @@ return (
 <div className="media-banner">
                 <div className="container justify-content-center">
                     <div className="about-banner-box ">
-                        <div className="col-5">
+                        <div className="col-12 col-sm-5">
                             <h1 className="about-banner-title">
                                 MEDIA
                             </h1>
@@ -17,8 +17,8 @@ return (
                             <path d="M4 4H172.899" stroke="white" stroke-width="7" stroke-linecap="round"/>
                             </svg>
                         </div>
-                        <div className="col-7">
-                            <p className="about-banner-quote">
+                        <div className="col-sm-12 col-7">
+                            <p className="media-banner-quote">
                             Discover the latest news, updates, and stories relating to the world of solar energy and 
                             how it is the key towards a sustainable future. Follow our media updates to witness the 
                             remarkable stories of sustainability and growth. 
