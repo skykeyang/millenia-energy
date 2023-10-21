674a3b5aacf50797707616c1dcc97daddc0fdad2
skykeyang <sohky98@gmail.com>
2023-10-22 07:19:09 +0800

    Update home_banner.js

--- src/components/home/home_banner.js
+++ src/components/home/home_banner.js
@@ -13,9 +13,9 @@ export default function HomeBanner({children}) {
     return (
     <div className='banner-wrapper gx-0'>
         {children}
-        <video ref={videoRef} autoPlay loop muted  className='homebanner' preload>
-            {/* <source src="https://millenia-energy-asset.s3.ap-southeast-1.amazonaws.com/solarbackgroundmoving.mp4" type='video/mp4' /> */}
-            <source autoPlay={true} muted={true} playsInline src={require("../../styles/solarbackgroundmoving.mp4").default} type='video/mp4' />
+        <video ref={videoRef} autoPlay loop muted className='homebanner' preload>
+            <source playsInline muted src="https://millenia-energy-asset.s3.ap-southeast-1.amazonaws.com/solarbackgroundmoving.mp4" type='video/mp4' />
+            {/* <source  muted playsInline src={require("../../styles/solarbackgroundmoving.mp4").default} type='video/mp4' /> */}
         </video>
         
     </div>
